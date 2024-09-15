64f3795742364dac6876917de13b01326c6ebf06
mostafa <mostafa.osman.fathi@gmail.com>
2024-09-16 00:17:37 +0300

    adding Problem-Solving and LeetCode Array one

---
+++ Problem-Solving/LeetCode/Array-One/cells-with-odd-values-in-a-matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+       int[] row = new int[m], col = new int[n];
+       int res = 0;
+       for (int[] v : indices) {
+           ++row[v[0]];
+           ++col[v[1]];
+       }
+       for (int i = 0; i < m; i++) {
+           for (int j = 0; j < n; j++) {
+               if (((row[i]+col[j])&1)!=0){
+                   res += 1;
+               }
+           }
+       }
+       return res;
+   }
+}
